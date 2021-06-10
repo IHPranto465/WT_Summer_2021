6bbf89460a4ab8f83f13417db31461a580c7bdba
Mr. Ismail Hossain <64597455+Ismail12318@users.noreply.github.com>
2021-06-10 22:55:06 +0600

    Add files via upload

---
+++ Task-1/reg1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<form>
+<tr>
+<th> Full Name: </th>
+<th><input type="text" name="firstname" size="15"/></th><br>
+</tr>
+
+<br>
+
+<tr>
+ <th> <label>E - mail:--</label></th>
+<th><input type="text" name="E-mail" size="15"/></th><br>
+</tr>
+
+<br>
+
+<tr>
+<th>Password:</th>    
+<th><input type="text" name="Password" size="15"/></th> <br>
+</tr>
+
+
+
+<br> 
+<tr>
+Comment: 
+<td><textarea cols="80" rows="5" value="Comment">
+</textarea></td>
+<br>
+
+Gender :  
+</label>
+<input type="radio" name="male"/> Male 
+<input type="radio" name="female"/> Female
+<input type="radio" name="other"/> Other  
+<br>  
+<label>  
+Please choose a Hobby
+</label>
+<input type="checkbox" name="Sing" value="Singing"> Singing
+<input type="checkbox" name="Dance" value="Dancing"> Dancing
+<input type="checkbox" name="Read" value="Reading"> Reading <br>
+
+Please choose a file
+<input type="file" id="myFile" name="filename">
+
+  <br>
+  <input type="submit" value="submit">
+  <input type ="reset">
+  
+
+</form> 
+</tr> 
+</body>  
+</html>  
