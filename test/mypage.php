7864abf0d85936d42e045a1cb8f275d9c1d9bcf9
Mr. Ismail Hossain <64597455+Ismail12318@users.noreply.github.com>
2021-06-10 22:54:16 +0600

    Add files via upload

---
+++ test/mypage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Home Page</title>
+</head>
+<body>
+<?php
+$txt = "Hello world!";
+echo "My php code " . $txt . "!";
+$x = 5;
+$y = 5.2;
+echo $x + $y;
+?>
+  My Home Page
+  My profile 
+</body>
